132f44f964bd4cb8367eb1bb0ec4abb25129e800
BluePandora <bluepandora666@gmail.com>
2015-03-10 22:22:19 +0600

    testing commit

--- src/main/resources/com/betelguese/servlet/BSMService.java
+++ src/main/resources/com/betelguese/servlet/BSMService.java
@@ -46,8 +46,8 @@ public final class BSMService extends HttpServlet {
 		// String serverProcessedService = requestProcess.getRequestProcess(
 		// request, response);
 		// response.setContentType("text/html;charset=UTF-8");
-		// PrintWriter out = response.getWriter();
-		// out.println(serverProcessedService);
+		 PrintWriter out = response.getWriter();
+		 out.println(serverProcessedService);
 	}
 
 	/**
